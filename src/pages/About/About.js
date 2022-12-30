a8e31d45f561f08e5e743180aa6764e720bb920b
Ma'rufjon <m.mamatkulov95@gmail.com>
2022-12-30 10:15:49 +0500

    [About] -Created and developed about page of project

---
+++ src/pages/About/About.js
@@ -0,0 +1,27 @@
+import "./About.css";
+
+export default function About() {
+  return (
+    <div className="about">
+      <div className="team">
+        <div className="team-greeting">
+          <h1>WE ARE TEAM ARCHI'S</h1>
+          <p className="team-description">
+            A team of super cool people who have come together to impart the
+            skills which have been absent around here. Wondering how? We are
+            equipped with cool powers which are transferable!
+          </p>
+        </div>
+        <div className="team-purpose">
+          <h1>Why are we here</h1>
+          <p className="team-description">
+            We want to change the way self-development is done. After all, we
+            have some cool mentors to assist you in the development process! So,
+            we decided to prepare the workforce of tomorrow with our traineeship
+            program!
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+}
---
+++ src/pages/Cart/Cart.js
@@ -0,0 +1,5 @@
+import "./Cart.css";
+
+export default function () {
+  return <div className="cart"></div>;
+}
